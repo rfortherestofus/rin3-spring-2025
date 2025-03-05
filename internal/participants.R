cd97c0c45537db84c1a1e71d326d1cb18fd5458e
David Keyes <david@rfortherestofus.com>
2025-03-04 19:09:01 -0800

    Create participants.R

---
+++ internal/participants.R
@@ -0,0 +1,7 @@
+library(rfortherestofus)
+
+post_course_participants(
+  course_title = "R in 3 Months (Spring 2025)",
+  sheet_name = "Spring 2025 Participants",
+  google_sheets_url = "https://docs.google.com/spreadsheets/d/1Rz7_1_CzxURdhPHRj9YAzV-tgQP6xQmY_qV0VPjhPYs/edit?gid=812055131#gid=812055131"
+)
