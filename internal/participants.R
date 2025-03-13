a39d9c151145c70d50d670799980b7968fc41169
David Keyes <david@rfortherestofus.com>
2025-03-13 11:04:10 -0700

    Post week 1

--- internal/participants.R
+++ internal/participants.R
@@ -1,3 +1,4 @@
+library(tidyverse)
 library(rfortherestofus)
 
 post_course_participants(
@@ -5,3 +6,8 @@ post_course_participants(
   sheet_name = "Spring 2025 Participants",
   google_sheets_url = "https://docs.google.com/spreadsheets/d/1Rz7_1_CzxURdhPHRj9YAzV-tgQP6xQmY_qV0VPjhPYs/edit?gid=812055131#gid=812055131"
 )
+
+rin3_participants <-
+  get_course_participants(
+    course_title = "R in 3 Months (Spring 2025)"
+  )
