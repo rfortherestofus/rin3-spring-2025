09540a5d77f55e913ceaedd4cf6c559b56b45df0
David Keyes <david@rfortherestofus.com>
2025-05-21 15:06:11 -0700

    Week 11 prep

--- misc/mapping.R
+++ misc/mapping.R
@@ -9,42 +9,13 @@ portland_boundaries <-
   clean_names() |>
   filter(cityname == "Portland")
 
-ggplot(data = portland_boundaries) +
-  geom_sf()
-
 traffic_signals <-
   read_sf("data-raw/Traffic_Signals.geojson") |>
   clean_names()
 
-ggplot(data = traffic_signals) +
-  geom_sf()
-
 snow_and_ice_routes <-
   read_sf("data-raw/Snow_and_Ice_Routes.geojson") |>
   clean_names()
-
-ggplot(data = snow_and_ice_routes) +
-  geom_sf()
-
-ggplot() +
-  geom_sf(
-    data = portland_boundaries
-  ) +
-  geom_sf(
-    data = snow_and_ice_routes,
-    aes(
-      color = special_area
-    )
-  ) +
-  geom_sf(
-    data = traffic_signals,
-    aes(
-      color = is_metered
-    ),
-    alpha = 0.5,
-    size = 1
-  ) +
-  theme_void()
 
 # Tigris ------------------------------------------------------------------
 
@@ -54,13 +25,7 @@ us_states <- states()
 
 us_states
 
-ggplot(data = us_states) +
-  geom_sf()
-
 oregon_counties <- counties(state = "Oregon")
-
-ggplot(data = oregon_counties) +
-  geom_sf()
 
 # Median Income -----------------------------------------------------------
 
@@ -77,21 +42,7 @@ median_income <-
 
 median_income |>
   ggplot(aes(fill = estimate)) +
-  geom_sf() +
-  labs(
-    fill = NULL,
-    title = "Median Household Income by County"
-  ) +
-  scale_fill_viridis_c(
-    option = "magma",
-    labels = dollar_format()
-  ) +
-  theme_void() +
-  theme(plot.title = element_text(
-    face = "bold",
-    hjust = 0.5
-  ))
-
+  geom_sf()
 
 
 # International Data ------------------------------------------------------
@@ -103,8 +54,23 @@ iceland <-
     country = "Iceland",
     scale = "large",
     returnclass = "sf"
-  ) |> 
+  ) |>
   select(sovereignt)
 
 ggplot(data = iceland) +
   geom_sf()
+
+
+# Interactive -------------------------------------------------------------
+
+library(ggiraph)
+
+median_income_interactive_plot <-
+  median_income |>
+  ggplot(aes(
+    fill = estimate,
+    tooltip = NAME
+  )) +
+  geom_sf_interactive()
+
+girafe(ggobj = median_income_interactive_plot)
--- misc/theme-dk.R
+++ misc/theme-dk.R
@@ -1,46 +1,34 @@
-theme_dk <- function(base_font = "Inter",
-                     show_gridlines = TRUE,
-                     show_legend = TRUE) {
-  
-  custom_theme <- 
-  theme_minimal(base_family = base_font) +
-    theme(
-      axis.title = element_blank(),
-      axis.text = element_text(
-        color = "grey60",
-        size = 18
-      )
-    )
-  
-  if (show_gridlines == FALSE) {
-    custom_theme <- custom_theme +
-      theme(
-        panel.grid = element_blank()
-      )
-  }
-  
-  if (show_legend == FALSE) {
-    custom_theme <- custom_theme +
-      theme(
-        legend.position = "none"
-      )
-  }
-  
-  return(custom_theme)
-}
-
 library(tidyverse)
+library(ggtext)
 library(palmerpenguins)
 
-penguins |>
-  count(island) |>
+
+theme_dk <- function() {
+  theme_minimal() +
+    theme(
+      axis.title = element_blank(),
+      plot.title = element_markdown(),
+      plot.title.position = "plot",
+      panel.grid = element_blank(),
+      axis.text = element_text(color = "grey60", size = 10)
+    )
+}
+
+penguin_bar_chart <-
+  penguins |>
+  group_by(island) |>
+  summarize(mean_bill_length = mean(bill_length_mm, na.rm = TRUE)) |>
   ggplot(
     aes(
       x = island,
-      y = n,
+      y = mean_bill_length,
+      label = island,
       fill = island
     )
   ) +
-  geom_col() +
-  theme_dk(show_gridlines = FALSE,
-           show_legend = FALSE)
+  geom_col()
+
+penguin_bar_chart
+
+penguin_bar_chart +
+  theme_dk()
